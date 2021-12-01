d6495cb4f2dfd2e44d841898c9b270221d0fad04
Joshua Maddox <65622463+jpmaddox97@users.noreply.github.com>
2021-12-01 16:26:12 -0500

    Updated comments

--- gol.py
+++ gol.py
@@ -11,27 +11,23 @@ import argparse
 import time
 
 
-
+# Class to create the Board object and redrawing of it
 class Board(object):
+    
+    # Initialize the initial seed the engine class and the first iteration
     def __init__(self, size, seed = 'Random'):
         if seed == 'Random':
             self.state = np.random.randint(2, size = size)
         self.engine = Engine(self)
         self.iteration = 0
 
-    # def _save_image(self, image):
-    #     print(image)
-    #     # image.savefig('NFT.png')
-
-    # def _get_scene(self, plot):
-    #     image = plot
-    #     return False, image
-
+    # Pause the animation state to allow user to save image
     def _animation_sate(self, i):
         if i == 50:
             plt.pause(15)
             return False
 
+    # Animate the game of life using matplotlip.pyplot
     def animate(self):
         i = self.iteration
         im = None
@@ -39,6 +35,8 @@ class Board(object):
         while True:
             if i == 0:
                 plt.ion()
+                
+                # cmap was manually changed for each NFT, would like to automate in the future
                 im = plt.imshow(self.state, vmin = 0, vmax = 2, cmap = 'gist_ncar')
             else:
                 im.set_data(self.state)
@@ -49,17 +47,17 @@ class Board(object):
             i += 1
             self.engine.applyRules()
             
+            # Print to console
             print('Life Cycle: {} Birth: {} Survive: {}'.format(i, self.engine.nBirth, self.engine.nSurvive))
             plt.pause(0.01)
             yield self  
 
-        # image = self._get_scene(im)
-        # self._save_image(image)
-
+# Engine that creates the logic of the game     
 class Engine(object):
     def __init__(self, board):
         self.state = board.state
     
+    # Check to see if there are neighbors around given cell and returns number of neighbors
     def countNeighbors(self):
         state = self.state
         n = (state[0:-2,0:-2] + state[0:-2,1:-1] + state[0:-2,2:] +
@@ -67,6 +65,7 @@ class Engine(object):
             state[2:,1:-1] + state[2:,2:])
         return n
     
+    # Apply the game logic to the given cell based on the number of neighbors around it
     def applyRules(self):
         n = self.countNeighbors()
         state = self.state
@@ -80,6 +79,7 @@ class Engine(object):
         self.nSurvive = nSurvive
         return state
 
+# Main function to run the game with animation
 def main():
     ap = argparse.ArgumentParser(add_help = False) # Intilialize Argument Parser
     ap.add_argument('-h', '--height', help = 'Board Height', default = 256)
@@ -89,7 +89,6 @@ def main():
     bWidth = int(args['width'])
     board = Board((bHeight,bWidth))
     for _ in board.animate():
-        print(_)
         pass
 
 if __name__ == '__main__':
